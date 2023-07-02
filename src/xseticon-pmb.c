c1a34e872fa815794b59a09aedd091cf22a5c0db
mk-pmb <mk-pmb@users.noreply.github.com>
2023-07-02 23:13:56 +0200

    Try explicitly releasing XSynchronize before XCloseDisplay.

--- src/xseticon-pmb.c
+++ src/xseticon-pmb.c
@@ -206,6 +206,7 @@ int main(int argc, char* argv[]) {
 
   if(!result) { failed("XChangeProperty"); }
   if(!XFlush(display)) { failed("XFlush"); }
+  XSynchronize(display, FALSE);
   XCloseDisplay(display);
   return 0;
 }
